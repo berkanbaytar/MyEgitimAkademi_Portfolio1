4aadc578920e238b95955146ff584af2e7f6c3d7
Berkan BAYTAR <143795341+berkanbaytar@users.noreply.github.com>
2023-09-16 19:17:24 +0300

    for github

--- MyEgitimAkademi_Portfolio1/Controllers/ProjectController.cs
+++ MyEgitimAkademi_Portfolio1/Controllers/ProjectController.cs
@@ -10,10 +10,50 @@ namespace MyEgitimAkademi_Portfolio1.Controllers
     public class ProjectController : Controller
     {
         MyPortfolioDb1Entities db = new MyPortfolioDb1Entities();
+        [Authorize]
         public ActionResult Index()
         {
             var values = db.Project.ToList();
             return View(values);
         }
+            public ActionResult AddProject()
+            {
+                return View();
+            }
+            [HttpPost]
+            public ActionResult AddProject(Project project)
+            {
+                db.Project.Add(project);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            public ActionResult DeleteProject(int id)
+            {
+                var value = db.Project.Find(id);
+                db.Project.Remove(value);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+        }
+            [HttpGet]
+
+            public ActionResult UpdateProject(int id)
+            {
+                var value = db.Project.Find(id);
+                return View(value);
+            }
+            [HttpPost]
+
+            public ActionResult UpdateProject(Project project)
+            {
+                var value = db.Project.Find(project.ProjectID);
+                value.Title = project.Title;
+                value.Description = project.Description;
+                value.CompleteDay = project.CompleteDay;
+                value.Price = project.Price;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
     }
-}
--- MyEgitimAkademi_Portfolio1/Controllers/ServiceController.cs
+++ MyEgitimAkademi_Portfolio1/Controllers/ServiceController.cs
@@ -11,6 +11,7 @@ namespace MyEgitimAkademi_Portfolio1.Controllers
     {
 
         MyPortfolioDb1Entities db = new MyPortfolioDb1Entities();
+        [Authorize]
         public ActionResult Index()
         {
             var values = db.Service1.ToList();
@@ -19,6 +20,37 @@ namespace MyEgitimAkademi_Portfolio1.Controllers
         public ActionResult AddService()
         {
             return View();
+        }
+        [HttpPost]
+
+        public ActionResult AddService(Service1 service)
+        {
+            db.Service1.Add(service);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteService(int id)
+        {
+            var value = db.Service1.Find(id);
+            db.Service1.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public ActionResult UpdateService(int id)
+        {
+            var value = db.Service1.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateService(Service1 service)
+        {
+            var value = db.Service1.Find(service.ServiceID);
+            value.ServiceName = service.ServiceName;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
